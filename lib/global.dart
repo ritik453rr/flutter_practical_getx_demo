6445dec1a678ca787861149305bc3baf811093d1
Ritik <dritik458@gmail.com>
2025-03-07 22:43:49 +0530

    Added internet check

--- lib/global.dart
+++ lib/global.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 
 class Global {
@@ -6,4 +7,17 @@ class Global {
   static hideKeyBoard(){
   FocusManager.instance.primaryFocus?.unfocus();
   }
+
+  
+/// Function to check the internet connection using the connectivity_plus package
+  static Future<bool> checkInternetConnection() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult.contains(ConnectivityResult.mobile) ||
+        connectivityResult.contains(ConnectivityResult.wifi) || connectivityResult.contains(ConnectivityResult.ethernet)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
--- lib/screens/splash/splash_controller.dart
+++ lib/screens/splash/splash_controller.dart
@@ -1,16 +1,22 @@
 import 'package:get/get.dart';
+import 'package:getx_demo/global.dart';
 import 'package:getx_demo/routing/app_routes.dart';
 
 class SplashController extends GetxController {
   @override
   void onInit() {
     super.onInit();
+
     navigation();
   }
 
   void navigation() {
-    Future.delayed(const Duration(seconds: 2), () {
-      Get.toNamed(AppRoutes.routeVerifyPhone);
+    Future.delayed(const Duration(seconds: 2), () async {
+      if (await Global.checkInternetConnection()) {
+        Get.toNamed(AppRoutes.routeVerifyPhone);
+      } else {
+        print("No Internet....");
+      }
     });
   }
 }
